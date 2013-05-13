822e7fade105ab595150ac3c703443a68ca29b60
root <root@tolkien.tecnodz.com>
2013-05-13 20:30:50 -0300

    updating directory structure

--- htdocs/.php
+++ htdocs/.php
@@ -1,4 +1,4 @@
 <?php
-require_once dirname(dirname(__FILE__)).'/apps/lib/vendor/Birds/bird.php';
-Birds\bird::app('birds', 'dev')->fly();
+require_once dirname(dirname(__FILE__)).'/Birds/bird.php';
+Birds\bird::app()->fly();
 Birds\bird::log('-- '.substr(microtime(true) - BIRD_TIME,0,9).'s Mem: '.Birds\bird::bytes(memory_get_usage(),3).' / '.Birds\bird::bytes(memory_get_peak_usage(),3)." ".Birds\bird::scriptName());
